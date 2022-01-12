39b56d9c33875b767e1cb2b127bdb412881efdc7
Dilip D <dilipduraiswamy@gmail.com>
2022-01-12 13:18:55 +0530

    Update SampleProjApplication.java

--- src/main/java/com/example/SampleProjApplication.java
+++ src/main/java/com/example/SampleProjApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleProjApplication {
 
 	public static void main(String[] args) {
+		//adding comments
 		SpringApplication.run(SampleProjApplication.class, args);
 	}
 
