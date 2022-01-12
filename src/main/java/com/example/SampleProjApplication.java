c9e81b1f8fe75c8a901ea9928537c9dd27d740f4
dilipduraiswamy <dilipduraiswamy@gmail.com>
2022-01-12 13:23:12 +0530

    merged code changes

--- src/main/java/com/example/SampleProjApplication.java
+++ src/main/java/com/example/SampleProjApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleProjApplication {
 
 	public static void main(String[] args) {
+		//adding new comment line
 		//adding comments
 		SpringApplication.run(SampleProjApplication.class, args);
 	}
